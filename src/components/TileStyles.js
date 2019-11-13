841efcbffa227ec00f9b20d9cf8825ce3c584891
Zachariah Crowell <zachariah.crowell@techtonic.com>
2019-11-13 11:57:18 -0700

    Removed white text

--- src/components/TileStyles.js
+++ src/components/TileStyles.js
@@ -9,20 +9,22 @@ const styles = {
      flex-direction: column;
      justify-content: center;
      align-items: center;
-     border: 3px solid dodgerblue;
-     color: white;
+     border: 4px solid dodgerblue;
      margin: 5px;
+     cursor: pointer;
 `,
     not : css`
     text-align: center;
      background-color: grey;
-     border: 3px solid grey;
+     border: 4px solid grey;
      height: 150px;
      display: flex;
      flex-direction: column;
      justify-content: center;
      align-items: center;
-    margin: 5px;
+     margin: 5px;
+     cursor: pointer;
+
 `
 }
 export default styles
