cc8491a213e43af0e713df5dcbd817f87842f2d8
Zachariah Crowell <zachariah.crowell@techtonic.com>
2019-11-13 12:19:48 -0700

    Moved allAnimals mock data to it's own file

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,18 +2,30 @@
 import { useState } from "react";
 import { jsx } from "@emotion/core";
 import Tile from "./Tile.js";
+import allAnimals from "./animalsData.js";
 // Our starting with an array of multiple animal objects
-const allAnimals = [
-  { id: 1, 
-    name: "Dog", 
-  description: "Lorem ipsum dolor amet vHS PBR&B semiotics schlitz tacos. Williamsburg umami art party chillwave asymmetrical food truck echo park venmo distillery truffaut. Kale chips retro salvia taxidermy lomo kombucha, hoodie kinfolk organic tilde pop-up yr truffaut raclette PBR&B. 90's readymade microdosing occupy migas 8-bit. Vinyl craft beer DIY tofu slow-carb try-hard. Sriracha meh PBR&B taiyaki fixie raw denim cloud bread taxidermy ugh blue bottle meggings scenester put a bird on it cliche gastropub." },
-  { id: 2, 
-    name: "Monkey", 
-  description: "Direct trade microdosing green juice, dreamcatcher tacos photo booth deep v knausgaard intelligentsia mlkshk. Venmo cronut retro, pug meggings thundercats fam dreamcatcher iPhone tattooed. Kogi austin shabby chic occupy, tumblr jianbing bicycle rights raw denim paleo marfa next level unicorn vape. Flexitarian waistcoat locavore, pug lumbersexual YOLO affogato." },
-  { id: 3, 
-    name: "Cat", 
-  description: "+1 hot chicken fingerstache street art, cred ethical retro tacos meditation letterpress pok pok everyday carry truffaut paleo. Semiotics lomo schlitz celiac shabby chic scenester flannel food truck small batch disrupt sustainable franzen deep v tumeric farm-to-table. Enamel pin blue bottle put a bird on it ugh swag waistcoat sriracha hella synth. Before they sold out cloud bread tbh organic hot chicken lumbersexual, everyday carry skateboard leggings waistcoat bicycle rights godard typewriter normcore bitters. Semiotics food truck lomo gluten-free salvia. Crucifix keffiyeh sriracha chia roof party leggings. PBR&B hexagon shaman organic venmo distillery, mlkshk lyft tote bag sriracha tattooed art party." }
-];
+
+
+
+/* 
+below is what the mock data imported from allAnimals looks like
+*/
+
+
+
+// const allAnimals = [
+//   { id: 1, 
+//     name: "Dog", 
+//   description: "Lorem ipsum dolor amet vHS PBR&B semiotics schlitz tacos. Williamsburg umami art party chillwave asymmetrical food truck echo park venmo distillery truffaut. Kale chips retro salvia taxidermy lomo kombucha, hoodie kinfolk organic tilde pop-up yr truffaut raclette PBR&B. 90's readymade microdosing occupy migas 8-bit. Vinyl craft beer DIY tofu slow-carb try-hard. Sriracha meh PBR&B taiyaki fixie raw denim cloud bread taxidermy ugh blue bottle meggings scenester put a bird on it cliche gastropub." },
+//   { id: 2, 
+//     name: "Monkey", 
+//   description: "Direct trade microdosing green juice, dreamcatcher tacos photo booth deep v knausgaard intelligentsia mlkshk. Venmo cronut retro, pug meggings thundercats fam dreamcatcher iPhone tattooed. Kogi austin shabby chic occupy, tumblr jianbing bicycle rights raw denim paleo marfa next level unicorn vape. Flexitarian waistcoat locavore, pug lumbersexual YOLO affogato." },
+//   { id: 3, 
+//     name: "Cat", 
+//   description: "+1 hot chicken fingerstache street art, cred ethical retro tacos meditation letterpress pok pok everyday carry truffaut paleo. Semiotics lomo schlitz celiac shabby chic scenester flannel food truck small batch disrupt sustainable franzen deep v tumeric farm-to-table. Enamel pin blue bottle put a bird on it ugh swag waistcoat sriracha hella synth. Before they sold out cloud bread tbh organic hot chicken lumbersexual, everyday carry skateboard leggings waistcoat bicycle rights godard typewriter normcore bitters. Semiotics food truck lomo gluten-free salvia. Crucifix keffiyeh sriracha chia roof party leggings. PBR&B hexagon shaman organic venmo distillery, mlkshk lyft tote bag sriracha tattooed art party." }
+// ];
+
+
 // Our starting current animal
 const currentAnimal = { id: 2, name: "Monkey" };
 console.log("Current animal", currentAnimal)
---
+++ src/components/animalsData.js
@@ -0,0 +1,13 @@
+const allAnimals = [
+    { id: 1, 
+      name: "Dog", 
+    description: "Lorem ipsum dolor amet vHS PBR&B semiotics schlitz tacos. Williamsburg umami art party chillwave asymmetrical food truck echo park venmo distillery truffaut. Kale chips retro salvia taxidermy lomo kombucha, hoodie kinfolk organic tilde pop-up yr truffaut raclette PBR&B. 90's readymade microdosing occupy migas 8-bit. Vinyl craft beer DIY tofu slow-carb try-hard. Sriracha meh PBR&B taiyaki fixie raw denim cloud bread taxidermy ugh blue bottle meggings scenester put a bird on it cliche gastropub." },
+    { id: 2, 
+      name: "Monkey", 
+    description: "Direct trade microdosing green juice, dreamcatcher tacos photo booth deep v knausgaard intelligentsia mlkshk. Venmo cronut retro, pug meggings thundercats fam dreamcatcher iPhone tattooed. Kogi austin shabby chic occupy, tumblr jianbing bicycle rights raw denim paleo marfa next level unicorn vape. Flexitarian waistcoat locavore, pug lumbersexual YOLO affogato." },
+    { id: 3, 
+      name: "Cat", 
+    description: "+1 hot chicken fingerstache street art, cred ethical retro tacos meditation letterpress pok pok everyday carry truffaut paleo. Semiotics lomo schlitz celiac shabby chic scenester flannel food truck small batch disrupt sustainable franzen deep v tumeric farm-to-table. Enamel pin blue bottle put a bird on it ugh swag waistcoat sriracha hella synth. Before they sold out cloud bread tbh organic hot chicken lumbersexual, everyday carry skateboard leggings waistcoat bicycle rights godard typewriter normcore bitters. Semiotics food truck lomo gluten-free salvia. Crucifix keffiyeh sriracha chia roof party leggings. PBR&B hexagon shaman organic venmo distillery, mlkshk lyft tote bag sriracha tattooed art party." }
+  ];
+
+  export default allAnimals
